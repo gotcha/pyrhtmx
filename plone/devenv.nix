626ae5a82ab7d80e8944062a5a8c2a5b4b79f130
Godefroid Chapelle <gotcha@bubblenet.be>
2024-06-13 12:41:22 +0200

    first import

---
+++ plone/devenv.nix
@@ -0,0 +1,40 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "plone devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  languages.python.enable = true;
+  languages.python.uv.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = "-r requirements.txt";
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
